67923ae0a5fab23b16164a6f57e210b1dffb4462
Ray T <112096459+TelRay701@users.noreply.github.com>
2023-01-08 18:38:33 -0500

    Updates

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -10,12 +10,10 @@ class Product extends React.Component {
   };
 
   componentDidMount() {
-    axios
-      .get("http://ec2-44-208-165-95.compute-1.amazonaws.com:3001/Products")
-      .then((res) => {
-        const items = res.data;
-        this.setState({ products: items });
-      });
+    axios.get("/Products").then((res) => {
+      const items = res.data;
+      this.setState({ products: items });
+    });
   }
 
   myFilter(type) {
